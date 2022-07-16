f91e906c01d8c3301d80493595c8e32e70f13ace
INCREDIBLE POSISI <87313549+bulanbintang69@users.noreply.github.com>
2022-07-17 02:01:34 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -87,6 +87,7 @@ async def start_command(client: Client, message: Message):
                 pass
         return
     else:
+       temp_msg = await message.reply_sticker(sticker="CAACAgUAAxkBAAEIOCti0wczUvZ-s7T0ryPcej-1f0cNBQAC5QYAAmhFmFZSkAc3l4QhEikE", quote=True)
         reply_markup = InlineKeyboardMarkup(
             [
                 [
@@ -98,8 +99,7 @@ async def start_command(client: Client, message: Message):
                 ]
             ]
         )
-       await message.reply_sticker(sticker="CAACAgUAAxkBAAEIOCti0wczUvZ-s7T0ryPcej-1f0cNBQAC5QYAAmhFmFZSkAc3l4QhEikE", quote=True)
-        await message.reply_text(
+     await message.reply_text(
             text = START_MSG.format(
                 first = message.from_user.first_name,
                 last = message.from_user.last_name,
@@ -112,7 +112,7 @@ async def start_command(client: Client, message: Message):
             quote = True
         )
         return
-    await message.reply_sticker.delete(15)
+    await temp_msg.delete(15)
 
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
