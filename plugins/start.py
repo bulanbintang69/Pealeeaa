cd418f24356844facff2b10489ee2daff84bd357
INCREDIBLE POSISI <87313549+bulanbintang69@users.noreply.github.com>
2022-07-17 01:59:14 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -98,7 +98,7 @@ async def start_command(client: Client, message: Message):
                 ]
             ]
         )
-       temp_msg = await message.reply_sticker(sticker="CAACAgUAAxkBAAEIOCti0wczUvZ-s7T0ryPcej-1f0cNBQAC5QYAAmhFmFZSkAc3l4QhEikE", quote=True)
+       await message.reply_sticker(sticker="CAACAgUAAxkBAAEIOCti0wczUvZ-s7T0ryPcej-1f0cNBQAC5QYAAmhFmFZSkAc3l4QhEikE", quote=True)
         await message.reply_text(
             text = START_MSG.format(
                 first = message.from_user.first_name,
@@ -112,7 +112,7 @@ async def start_command(client: Client, message: Message):
             quote = True
         )
         return
-    await temp_msg.delete(15)
+    await message.reply_sticker.delete(15)
 
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
